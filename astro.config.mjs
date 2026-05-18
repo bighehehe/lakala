f4f6a92c06545d409b3a2f861faabcd47a13d06d
bighehehe <1960075756@qq.com>
2026-05-18 10:48:25 +0800

    chore: 更新astro配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.pospee.com',
   output: 'static',
+  integrations: [sitemap()],
   devToolbar: {
     enabled: false
   },
